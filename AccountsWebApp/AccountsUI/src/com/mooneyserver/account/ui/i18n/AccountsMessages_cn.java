c7bf17d46a6d9a82f9ef3234f64b501c53baa232
Brian Mooney <brian_satchwannabe@hotmail.com>
2012-07-11 11:03:18 +0200

    Localization Fix

--- AccountsWebApp/AccountsUI/src/com/mooneyserver/account/ui/i18n/AccountsMessages_cn.java
+++ AccountsWebApp/AccountsUI/src/com/mooneyserver/account/ui/i18n/AccountsMessages_cn.java
@@ -11,14 +11,14 @@ public class AccountsMessages_cn extends AccountsMessages {
 	
 	static final Object[][] externStrings = {
 		/* Main Window Strings*/
-		{APP_TITLE, "会计"},
+		{APP_TITLE, "家庭财务"},
 		
 		/* Login Screen */
 		{LOGIN_BUTTON, "登录"},
-		{LOGIN_WELCOME, "会计软件欢迎您！"},
+		{LOGIN_WELCOME, "家庭财务软件欢迎您！"},
 		{USERNAME, "用户名"},
 		{PASSWORD, "密码"},
-		{FORGOT_PWD, "忘记我的密码"},
+		{FORGOT_PWD, "忘记密码"},
 		
 		/* Language Settings sub window */
 		{SELECT_LANGUAGE, "选择您的语言"}
