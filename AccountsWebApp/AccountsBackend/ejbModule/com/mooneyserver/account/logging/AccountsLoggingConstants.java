6869187b91162c6277366a87bec230c999a65ad6
brian_mooney <brian_mooney@dell.com>
2012-08-16 12:31:28 +0100

    Add more Log areas

--- AccountsWebApp/AccountsBackend/ejbModule/com/mooneyserver/account/logging/AccountsLoggingConstants.java
+++ AccountsWebApp/AccountsBackend/ejbModule/com/mooneyserver/account/logging/AccountsLoggingConstants.java
@@ -4,4 +4,5 @@ public class AccountsLoggingConstants {
 	public static final String LOG_AREA_UI = "[Log User Interface]";
 	public static final String LOG_AREA_JNDI_LOOKUP = "[Log JNDI Lookup]";
 	public static final String LOG_AREA_SETTINGS = "[Log Backend Settings]";
+	public static final String LOG_AREA_BACKEND = "[Log Backend Business Process]";
 }
