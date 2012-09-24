7768418b10e12210d20de7c0399df85afdb01a11
brian_mooney <brian_mooney@dell.com>
2012-09-24 13:30:47 +0100

    Fix for infinite recursion issue while closing sub windows

--- AccountsWebApp/AccountsUI/src/com/mooneyserver/account/ui/view/subwindow/BaseSubwindow.java
+++ AccountsWebApp/AccountsUI/src/com/mooneyserver/account/ui/view/subwindow/BaseSubwindow.java
@@ -33,4 +33,10 @@ public class BaseSubwindow extends Window implements IContainsCustomAnnotations 
 	public void loadBackendServices() {
         BackendServiceLookup.injectBackendServices(this);
     }
+	
+	@Override
+	public void close() {
+		super.close();
+		AccountsApplication.getInstance().nav.peek().refreshView();
+	}
 }
